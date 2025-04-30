35060fbec0c872470e68d5e8cd0ea925fd8deac5
Clifford Villanoy <villanoy.clifford@gmail.com>
2025-05-01 01:06:13 +0800

    Day 14: Python Project

---
+++ Dy 14/printVariables.py
@@ -0,0 +1,8 @@
+first_name = 'Clifford'
+last_name = 'Villanoy'
+country = 'Pholippines'
+
+print('First name:', first_name)
+print('Last name:', last_name)
+print('Country:', country)
+print('Full name:', first_name + ' ' + last_name)
