90f7735329e8d302ca9b505213d055e17c4378f5
Clifford Villanoy <villanoy.clifford@gmail.com>
2025-05-06 23:19:53 +0800

    Day 18: Python Project

---
+++ Day 18/sortList.py
@@ -0,0 +1,5 @@
+fruit_basket = ['Apple', 'Kiwi', 'Orange', 'Mango'] # List of fruits
+fruit_basket.sort() # Sort in ascending order
+print(fruit_basket) # Output: ['Apple', 'Kiwi', 'Mango', 'Orange']
+fruit_basket.sort(reverse=True) # Sort in descending order
+print(fruit_basket) # Output: ['Orange', 'Mango', 'Kiwi', 'Apple']      
