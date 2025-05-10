383ab809973463b9e33238ade5392526aea9b921
Clifford Villanoy <villanoy.clifford@gmail.com>
2025-05-11 00:02:37 +0800

    Day 21: Python Project

---
+++ Day 21/fString.py
@@ -0,0 +1,6 @@
+# List of fruits with their index
+fruits = ['apple', 'banana', 'cherry']
+
+for index, fruit in enumerate(fruits, start=1):
+    print(f"Fruit {index}: {fruit.capitalize()}")
+# Output:                   
